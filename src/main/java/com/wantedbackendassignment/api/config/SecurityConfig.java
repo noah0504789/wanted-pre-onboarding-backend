b60e1b38f774304300320e90fc11b23c2a889b50
noah0504789 <noah0504@naver.com>
2023-08-11 03:36:17 +0900

    Feat 2: configure spring security

--- src/main/java/com/wantedbackendassignment/api/config/SecurityConfig.java
+++ src/main/java/com/wantedbackendassignment/api/config/SecurityConfig.java
@@ -1,19 +1,40 @@
 package com.wantedbackendassignment.api.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wantedbackendassignment.api.auth.CustomExceptionHandlerFilter;
+import com.wantedbackendassignment.api.auth.login.LoginFailureHandler;
+import com.wantedbackendassignment.api.auth.login.LoginFilter;
+import com.wantedbackendassignment.api.auth.login.LoginProvider;
+import com.wantedbackendassignment.api.auth.login.LoginSuccessHandler;
 import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.header.HeaderWriterFilter;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
     private static String[] AUTHENTICATION_WHITELIST = null;
+    private final ObjectPostProcessor<Object> objectPostProcessor;
+    private final LoginProvider loginProvider;
+    private final ObjectMapper objectMapper;
+    private final LocalValidatorFactoryBean validator;
+    private final LoginSuccessHandler loginSuccessHandler;
+    private final LoginFailureHandler loginFailureHandler;
 
     @PostConstruct
     private void init() {
@@ -38,16 +59,45 @@ public class SecurityConfig {
         http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         http.authorizeHttpRequests(req ->  {
-            req
-                .requestMatchers(AUTHENTICATION_WHITELIST).permitAll()
-                .anyRequest().authenticated();
+            req.anyRequest().permitAll();
         });
+
+        CustomExceptionHandlerFilter exceptionHandlerFilter = createExceptionHandlerFilter();
+        LoginFilter loginFilter = createLoginFilter(authenticationManager(loginProvider));
+
+        http.addFilterBefore(exceptionHandlerFilter, HeaderWriterFilter.class)
+            .addFilterAfter(loginFilter, exceptionHandlerFilter.getClass());
 
         return http.build();
     }
 
     @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider... authenticationProviders) throws Exception {
+        AuthenticationManagerBuilder builder = new AuthenticationManagerBuilder(objectPostProcessor);
+
+        for (AuthenticationProvider provider : authenticationProviders) {
+            builder.authenticationProvider(provider);
+        }
+
+        return builder.build();
+    }
+
+    @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private LoginFilter createLoginFilter(AuthenticationManager authenticationManager) {
+        LoginFilter loginFilter = new LoginFilter(authenticationManager, objectMapper, validator);
+        loginFilter.setFilterProcessesUrl("/api/login");
+        loginFilter.setAuthenticationSuccessHandler(loginSuccessHandler);
+        loginFilter.setAuthenticationFailureHandler(loginFailureHandler);
+        loginFilter.afterPropertiesSet();
+
+        return loginFilter;
+    }
+
+    private CustomExceptionHandlerFilter createExceptionHandlerFilter() {
+        return new CustomExceptionHandlerFilter(objectMapper);
     }
 }
