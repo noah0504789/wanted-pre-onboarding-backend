3da93422fd0e3a7148a0987099dc5bb3312a18e0
noah0504789 <noah0504@naver.com>
2023-08-10 23:17:17 +0900

    config: database

--- src/main/java/com/wantedbackendassignment/api/WantedPreOnboardingBackendApplication.java
+++ src/main/java/com/wantedbackendassignment/api/WantedPreOnboardingBackendApplication.java
@@ -2,8 +2,10 @@ package com.wantedbackendassignment.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.wantedbackendassignment.api.properties")
 public class WantedPreOnboardingBackendApplication {
 
     public static void main(String[] args) {
